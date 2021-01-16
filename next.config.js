00494bd23cb49b593204db3ec58e38bf87499be3
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-01-16 16:06:07 +0100

    added i18n

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 const withOptimizedImages = require('next-optimized-images');
+const { nextI18NextRewrites } = require('next-i18next/rewrites');
+
+const localeSubpaths = {
+  de: 'de',
+  en: 'en',
+};
 
 module.exports = withOptimizedImages({
   optimizeImagesInDev: true,
   responsive: {
     adapter: require('responsive-loader/sharp'),
+  },
+  rewrites: async () => nextI18NextRewrites(localeSubpaths),
+  publicRuntimeConfig: {
+    localeSubpaths,
   },
 });
