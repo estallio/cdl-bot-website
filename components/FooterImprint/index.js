c6e77ae4daf067b24ea0eec792060fdfcb34c06e
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-01-20 16:40:52 +0100

    fixed translation

--- components/FooterImprint/index.js
+++ components/FooterImprint/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import useTranslation from 'next-translate/useTranslation';
+import { useTranslation } from '../../i18n';
 
 import styles from './FooterImprint.module.sass';
 
