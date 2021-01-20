5688953d8ebf25541ba3fe9cdb8d2cb645a97b6b
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-01-20 23:33:36 +0100

    added test open-graph

--- components/Meta/index.js
+++ components/Meta/index.js
@@ -52,7 +52,7 @@ const Meta = ({ title, description }) => {
           description: description,
           images: [
             {
-              url: 'https://cdl-bot-website.vercel.app/theme/images/logo.jpg',
+              url: 'https://cdl-bot-website.vercel.app/theme/images/logo.png',
               alt: 'CDL-BOT Logo',
             },
           ],
