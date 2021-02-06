8561973bdaf34b58ad23c73cf8e2350561ede74c
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-02-06 01:28:10 +0100

    fixed api to all content types

--- web/lib/api.js
+++ web/lib/api.js
@@ -1,28 +1,453 @@
 import { client } from './sanity';
 
+const fetchHome = async () => {
+  return await client.fetch(
+    `{
+      'footer': *[!(_id in path("drafts.**")) && _type == 'footer'][0]{ contact, impressDe, impressEn },
+      'seo': *[!(_id in path("drafts.**")) && _type == 'seo'][0]{ seoHome },
+      'home': *[!(_id in path("drafts.**")) && _type == 'home'][0]{
+        welcome{
+          ...,
+          'imageUrl': welcomeImage.asset->url,
+          welcomeTextEn[]{
+            ...,
+            _type == 'downloadButton' => {
+              'fileUrl': file.asset->url
+            },
+            _type == 'linkButton' => {
+              'linkUrl': href
+            },
+            _type == 'gallery' => {
+              images[]{
+                'imageUrl': asset->url
+              }
+            },
+            _type == 'image' => {
+              'imageUrl': asset->url
+            },
+            _type == 'block' => {
+              ...,
+              markDefs[] {
+                ...,
+                _type == 'link' => {
+                  'linkUrl': href
+                },
+                _type == 'file' => {
+                  'fileUrl': asset->url
+                },
+                _type == 'blueBoxLink' => {
+                  'linkUrl': href
+                },
+                _type == 'blueBoxFile' => {
+                  'fileUrl': asset->url
+                }
+              }
+            }
+          },
+          welcomeTextDe[]{
+            ...,
+            _type == 'downloadButton' => {
+              'fileUrl': file.asset->url
+            },
+            _type == 'linkButton' => {
+              'linkUrl': href
+            },
+            _type == 'gallery' => {
+              images[]{
+                'imageUrl': asset->url
+              }
+            },
+            _type == 'image' => {
+              'imageUrl': asset->url
+            },
+            _type == 'block' => {
+              ...,
+              markDefs[] {
+                ...,
+                _type == 'link' => {
+                  'linkUrl': href
+                },
+                _type == 'file' => {
+                  'fileUrl': asset->url
+                },
+                _type == 'blueBoxLink' => {
+                  'linkUrl': href
+                },
+                _type == 'blueBoxFile' => {
+                  'fileUrl': asset->url
+                }
+              }
+            }
+          }
+        },
+        research{
+          ...,
+          'imageUrl': researchImage.asset->url,
+          researchTextEn[]{
+            ...,
+            _type == 'downloadButton' => {
+              'fileUrl': file.asset->url
+            },
+            _type == 'linkButton' => {
+              'linkUrl': href
+            },
+            _type == 'gallery' => {
+              images[]{
+                'imageUrl': asset->url
+              }
+            },
+            _type == 'image' => {
+              'imageUrl': asset->url
+            },
+            _type == 'block' => {
+              ...,
+              markDefs[] {
+                ...,
+                _type == 'link' => {
+                  'linkUrl': href
+                },
+                _type == 'file' => {
+                  'fileUrl': asset->url
+                },
+                _type == 'blueBoxLink' => {
+                  'linkUrl': href
+                },
+                _type == 'blueBoxFile' => {
+                  'fileUrl': asset->url
+                }
+              }
+            }
+          },
+          researchTextDe[]{
+            ...,
+            _type == 'downloadButton' => {
+              'fileUrl': file.asset->url
+            },
+            _type == 'linkButton' => {
+              'linkUrl': href
+            },
+            _type == 'gallery' => {
+              images[]{
+                'imageUrl': asset->url
+              }
+            },
+            _type == 'image' => {
+              'imageUrl': asset->url
+            },
+            _type == 'block' => {
+              ...,
+              markDefs[] {
+                ...,
+                _type == 'link' => {
+                  'linkUrl': href
+                },
+                _type == 'file' => {
+                  'fileUrl': asset->url
+                },
+                _type == 'blueBoxLink' => {
+                  'linkUrl': href
+                },
+                _type == 'blueBoxFile' => {
+                  'fileUrl': asset->url
+                }
+              }
+            }
+          }
+        },
+        supportersAndPartners{
+          ...,
+          supportersAndPartnersTextEn[]{
+            ...,
+            _type == 'downloadButton' => {
+              'fileUrl': file.asset->url
+            },
+            _type == 'linkButton' => {
+              'linkUrl': href
+            },
+            _type == 'gallery' => {
+              images[]{
+                'imageUrl': asset->url
+              }
+            },
+            _type == 'image' => {
+              'imageUrl': asset->url
+            },
+            _type == 'block' => {
+              ...,
+              markDefs[] {
+                ...,
+                _type == 'link' => {
+                  'linkUrl': href
+                },
+                _type == 'file' => {
+                  'fileUrl': asset->url
+                },
+                _type == 'blueBoxLink' => {
+                  'linkUrl': href
+                },
+                _type == 'blueBoxFile' => {
+                  'fileUrl': asset->url
+                }
+              }
+            }
+          },
+          supportersAndPartnersTextDe[]{
+            ...,
+            _type == 'downloadButton' => {
+              'fileUrl': file.asset->url
+            },
+            _type == 'linkButton' => {
+              'linkUrl': href
+            },
+            _type == 'gallery' => {
+              images[]{
+                'imageUrl': asset->url
+              }
+            },
+            _type == 'image' => {
+              'imageUrl': asset->url
+            },
+            _type == 'block' => {
+              ...,
+              markDefs[] {
+                ...,
+                _type == 'link' => {
+                  'linkUrl': href
+                },
+                _type == 'file' => {
+                  'fileUrl': asset->url
+                },
+                _type == 'blueBoxLink' => {
+                  'linkUrl': href
+                },
+                _type == 'blueBoxFile' => {
+                  'fileUrl': asset->url
+                }
+              }
+            }
+          },
+          supportersAndPartners[]{
+            ...,
+            'linkUrl': href,
+            'imageUrl': logo.asset->url
+          }
+        }
+      }
+    }`
+  );
+};
+
 const fetchTeam = async () => {
   return await client.fetch(
-    `*[!(_id in path("drafts.**")) && _type == 'team'] { ..., 'pictureUrl': picture.asset->url } | order(order asc)`
+    `{
+      'footer': *[!(_id in path("drafts.**")) && _type == 'footer'][0]{ contact, impressDe, impressEn },
+      'seo': *[!(_id in path("drafts.**")) && _type == 'seo'][0]{ seoTeam },
+      'team': *[!(_id in path("drafts.**")) && _type == 'team'][0]{
+        persons[]{
+          ...,
+          'pictureUrl': picture.asset->url,
+          infoDe[]{
+            ...,
+            _type == 'downloadButton' => {
+              'fileUrl': file.asset->url
+            },
+            _type == 'linkButton' => {
+              'linkUrl': href
+            },
+            _type == 'gallery' => {
+              images[]{
+                'imageUrl': asset->url
+              }
+            },
+            _type == 'image' => {
+              'imageUrl': asset->url
+            },
+            _type == 'block' => {
+              ...,
+              markDefs[] {
+                ...,
+                _type == 'link' => {
+                  'linkUrl': href
+                },
+                _type == 'file' => {
+                  'fileUrl': asset->url
+                },
+                _type == 'blueBoxLink' => {
+                  'linkUrl': href
+                },
+                _type == 'blueBoxFile' => {
+                  'fileUrl': asset->url
+                }
+              }
+            }
+          },
+          infoEn[]{
+            ...,
+            _type == 'downloadButton' => {
+              'fileUrl': file.asset->url
+            },
+            _type == 'linkButton' => {
+              'linkUrl': href
+            },
+            _type == 'gallery' => {
+              images[]{
+                'imageUrl': asset->url
+              }
+            },
+            _type == 'image' => {
+              'imageUrl': asset->url
+            },
+            _type == 'block' => {
+              ...,
+              markDefs[] {
+                ...,
+                _type == 'link' => {
+                  'linkUrl': href
+                },
+                _type == 'file' => {
+                  'fileUrl': asset->url
+                },
+                _type == 'blueBoxLink' => {
+                  'linkUrl': href
+                },
+                _type == 'blueBoxFile' => {
+                  'fileUrl': asset->url
+                }
+              }
+            }
+          }
+        }
+      }
+    }`
   );
 };
 
 const fetchNews = async () => {
-  return await client.fetch(`*[!(_id in path("drafts.**")) && _type == "news"] {
-    ...,
-    newsEntryEn[]{
-      ...,
-      "imageUrl": asset->url
-      // also 'asset->' is possible to join the asset and gets all information like url etc.
-    }
-  } | order(date desc)`);
+  return await client.fetch(
+    `{
+      'footer': *[!(_id in path("drafts.**")) && _type == 'footer'][0]{ contact, impressDe, impressEn },
+      'seo': *[!(_id in path("drafts.**")) && _type == 'seo'][0]{ seoNews },
+      'news': *[!(_id in path("drafts.**")) && _type == 'news']{
+        date,
+        newsEntryDe[]{
+          ...,
+          _type == 'downloadButton' => {
+            'fileUrl': file.asset->url
+          },
+          _type == 'linkButton' => {
+            'linkUrl': href
+          },
+          _type == 'gallery' => {
+            images[]{
+              'imageUrl': asset->url
+            }
+          },
+          _type == 'image' => {
+            'imageUrl': asset->url
+          },
+          _type == 'block' => {
+            ...,
+            markDefs[] {
+              ...,
+              _type == 'link' => {
+                'linkUrl': href
+              },
+              _type == 'file' => {
+                'fileUrl': asset->url
+              },
+              _type == 'blueBoxLink' => {
+                'linkUrl': href
+              },
+              _type == 'blueBoxFile' => {
+                'fileUrl': asset->url
+              }
+            }
+          }
+        },
+        newsEntryEn[]{
+          ...,
+          _type == 'downloadButton' => {
+            'fileUrl': file.asset->url
+          },
+          _type == 'linkButton' => {
+            'linkUrl': href
+          },
+          _type == 'gallery' => {
+            images[]{
+              'imageUrl': asset->url
+            }
+          },
+          _type == 'image' => {
+            'imageUrl': asset->url
+          },
+          _type == 'block' => {
+            ...,
+            markDefs[] {
+              ...,
+              _type == 'link' => {
+                'linkUrl': href
+              },
+              _type == 'file' => {
+                'fileUrl': asset->url
+              },
+              _type == 'blueBoxLink' => {
+                'linkUrl': href
+              },
+              _type == 'blueBoxFile' => {
+                'fileUrl': asset->url
+              }
+            }
+          }
+        },
+      } | order(date desc)
+    }`
+  );
 };
 
 const fetchPublications = async () => {
-  const result = await client.fetch(
-    `*[!(_id in path("drafts.**")) && _type == 'publications']`
+  return await client.fetch(
+    `{
+      'footer': *[!(_id in path("drafts.**")) && _type == 'footer'][0]{ contact, impressDe, impressEn },
+      'seo': *[!(_id in path("drafts.**")) && _type == 'seo'][0]{ seoPublications },
+      'publications': *[!(_id in path("drafts.**")) && _type == 'publications'][0]{
+        content[]{
+          ...,
+          _type == 'downloadButton' => {
+            'fileUrl': file.asset->url
+          },
+          _type == 'linkButton' => {
+            'linkUrl': href
+          },
+          _type == 'gallery' => {
+            images[]{
+              'imageUrl': asset->url
+            }
+          },
+          _type == 'image' => {
+            'imageUrl': asset->url
+          },
+          _type == 'block' => {
+            ...,
+            markDefs[] {
+              ...,
+              _type == 'link' => {
+                'linkUrl': href
+              },
+              _type == 'file' => {
+                'fileUrl': asset->url
+              },
+              _type == 'blueBoxLink' => {
+                'linkUrl': href
+              },
+              _type == 'blueBoxFile' => {
+                'fileUrl': asset->url
+              }
+            }
+          }
+        }
+      }
+    }`
   );
-
-  return result[0];
 };
 
-export { fetchTeam, fetchNews, fetchPublications };
+export { fetchTeam, fetchNews, fetchPublications, fetchHome };
