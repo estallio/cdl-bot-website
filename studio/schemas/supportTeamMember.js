624ca356f72f07dccc77ee81411e84ff6dd4dde2
ElmiraEbrahimi <elmira.ebrahimi.1373@gmail.com>
2025-01-10 13:24:09 +0100

    feat: add schema for former administrative members

---
+++ studio/schemas/supportTeamMember.js
@@ -0,0 +1,17 @@
+export default {
+    title: 'Support Team Member',
+    name: 'supportTeamMember',
+    type: 'document',
+    fields: [
+      {
+        title: 'Name',
+        name: 'name',
+        type: 'string',
+      },
+      {
+        title: 'University',
+        name: 'university',
+        type: 'string',
+      },
+    ],
+  };
